9f9381c41475f2558f367f33638d830d81e01c8d
mmahammad57 <mmmdlimehemmed57@gmail.com>
2026-04-12 19:06:05 +0400

    Add MenuItem base class

--- src/model/MenuItem.java
+++ src/model/MenuItem.java
@@ -1,5 +1,32 @@
 package src.model;
+/**
+ * Abstract base class for all menu items.
+ * Each menu item has a name and defines its own resource requirements.
+ */
+public abstract class MenuItem {
 
-public class MenuItem {
-    
+    private String name;
+    private int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public abstract String getRequiredIngredients();
+
+    public abstract String getRequiredAppliance();
+
+    @Override
+    public String toString() {
+        return name + " ($" + price + ")";
+    }
 }
