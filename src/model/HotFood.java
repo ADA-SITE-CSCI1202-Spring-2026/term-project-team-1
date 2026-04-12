f331f8ad761201ca33477e0d6035773ec1e6bc04
mmahammad57 <mmmdlimehemmed57@gmail.com>
2026-04-12 19:11:54 +0400

    Add HotFood subclass extending MenuItem

--- src/model/HotFood.java
+++ src/model/HotFood.java
@@ -1,5 +1,24 @@
+/**
+ * Represents a hot food item such as a burger or fries.
+ * Requires the Grill appliance to process.
+ */
 package src.model;
+public class HotFood extends MenuItem {
 
-public class HotFood {
-    
+    private String mainIngredient;
+
+    public HotFood(String name, int price, String mainIngredient) {
+        super(name, price);
+        this.mainIngredient = mainIngredient;
+    }
+
+    @Override
+    public String getRequiredIngredients() {
+        return mainIngredient;
+    }
+
+    @Override
+    public String getRequiredAppliance() {
+        return "Grill";
+    }
 }
