c398dc62572b6fc5d64920306caf055dfd8dd2a9
iaghazada20562 <iaghazada20562@ada.edu.az>
2026-04-26 22:37:31 +0400

    Added basic RestockPanel with dropdown and Buy button

--- src/gui/Panels/Restockpanel.java
+++ src/gui/Panels/Restockpanel.java
@@ -1,5 +1,25 @@
 package src.gui.Panels;
 
-public class Restockpanel {
-    
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+public class RestockPanel extends VBox {
+
+    private ComboBox<String> ingredientDropdown;
+    private Button buyButton;
+
+    public RestockPanel() {
+
+        Label title = new Label("Restock Panel");
+        ingredientDropdown = new ComboBox<>();
+        ingredientDropdown.getItems().addAll("Buns", "Patties", "Vegetables");
+        buyButton = new Button("Buy Ingredient");
+        buyButton.setOnAction(e -> handleRestock());
+        getChildren().addAll(title, ingredientDropdown, buyButton);
+    }
+
+    private void handleRestock() {
+        String selected = ingredientDropdown.getValue();
+        System.out.println("Restocked: " + selected);
+    }
 }
